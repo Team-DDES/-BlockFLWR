caf13dcb5454950c586640c4eac341a831515e82
SuyeongOh <pental_vega@tvstorm.com>
2023-05-03 18:22:54 +0900

    main page search view 구현

---
+++ flutter_web/lib/data/bcfl.dart
@@ -0,0 +1,13 @@
+//TODO BCFL 관련 서버 response interface
+class BCFL{
+  var idx;
+  var taskName;
+  var owner;
+  var participants;
+  var intro;
+  var email;
+  var phone;
+  var framework;
+  var tokenSupply;
+  var dataType;
+}
--- flutter_web/lib/utils/text_utils.dart
+++ flutter_web/lib/utils/text_utils.dart
@@ -10,7 +10,16 @@ class TextUtils{
       ),
     );
   }
-
+  static Widget defaultTextWithSizeAlign(String content, double size, TextAlign align){
+    return Text(content,
+      textAlign: align,
+      style: TextStyle(
+        decoration: TextDecoration.none,
+        fontSize: size,
+        color: Colors.black,
+      ),
+    );
+  }
   static Widget defaultTextWithSizeColor(String content, double size, {required Color color}){
     return Text(content,
       style: TextStyle(
