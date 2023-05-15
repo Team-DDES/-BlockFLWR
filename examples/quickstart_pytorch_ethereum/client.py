cf3408a2a9d27dc9de53b914fc5f399765f7b9a6
Dae Yeol Kim <spicyyeol@gmail.com>
2023-05-15 18:22:55 +0900

    add model

--- examples/quickstart_pytorch_ethereum/client.py
+++ examples/quickstart_pytorch_ethereum/client.py
@@ -1,18 +1,13 @@
+import sys
 import warnings
 from collections import OrderedDict
+from logging import INFO
+
+import torch
 
 import flwr as fl
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from torchvision.datasets import CIFAR10
-from torchvision.transforms import Compose, Normalize, ToTensor
-from tqdm import tqdm
-from logging import DEBUG, INFO
 from flwr.common.logger import log
-
-import sys
+from . import *
 
 # #############################################################################
 # 1. Regular PyTorch pipeline: nn.Module, train, test, and DataLoader
@@ -21,59 +16,13 @@ import sys
 warnings.filterwarnings("ignore", category=UserWarning)
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-class Net(nn.Module):
-    """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
-
-    def __init__(self) -> None:
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        return self.fc3(x)
-
-
-def train(net, trainloader, epochs):
-    """Train the model on the training set."""
-    criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-    for _ in range(epochs):
-        for images, labels in tqdm(trainloader):
-            optimizer.zero_grad()
-            criterion(net(images.to(DEVICE)), labels.to(DEVICE)).backward()
-            optimizer.step()
-
-
-def test(net, testloader):
-    """Validate the model on the test set."""
-    criterion = torch.nn.CrossEntropyLoss()
-    correct, loss = 0, 0.0
-    with torch.no_grad():
-        for images, labels in tqdm(testloader):
-            outputs = net(images.to(DEVICE))
-            labels = labels.to(DEVICE)
-            loss += criterion(outputs, labels).item()
-            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
-    accuracy = correct / len(testloader.dataset)
-    return loss, accuracy
-
-
-def load_data():
-    """Load CIFAR-10 (training and test set)."""
-    trf = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    trainset = CIFAR10("./data", train=True, download=True, transform=trf)
-    testset = CIFAR10("./data", train=False, download=True, transform=trf)
-    return DataLoader(trainset, batch_size=32, shuffle=True), DataLoader(testset)
+if sys.argv[6] == "Cifar":
+    net = Cifar_Net().to(DEVICE)
+    train_loader, test_loader = Cifar_load_data()
+# elif sys.argv[6] == "Physnet":
+else:
+    net = PhysNet().to(DEVICE)
+    train_loader, test_laoder = PhysNet_load_data()
 
 
 # #############################################################################
@@ -81,11 +30,9 @@ def load_data():
 # #############################################################################
 
 # Load model and data (simple CNN, CIFAR-10)
-net = Net().to(DEVICE)
-trainloader, testloader = load_data()
 
-#sys[1] : server_address 127.0.0.1:8081
-#sys[2] : client cid
+# sys[1] : server_address 127.0.0.1:8081
+# sys[2] : client cid
 # sys[3] : contract address
 # sys[4] : token_address
 # sys[5] : nft_address
@@ -96,12 +43,11 @@ class FlowerClient(fl.client.EthClient):
     def __init__(self,
                  cid: str,
                  ):
-        super(FlowerClient, self).__init__(cid,sys.argv[3],sys.argv[4],sys.argv[5])
+        super(FlowerClient, self).__init__(cid, sys.argv[3], sys.argv[4], sys.argv[5])
 
         self.net = net
         self.IPFSClient.set_model(net)
         self.initial_setting()
-
 
     def get_parameters(self, config):
         return [val.cpu().numpy() for _, val in net.state_dict().items()]
@@ -110,7 +56,6 @@ class FlowerClient(fl.client.EthClient):
         params_dict = zip(net.state_dict().keys(), parameters)
         state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
         net.load_state_dict(state_dict, strict=True)
-
 
     def fit(self, config):
         print("Client FIT @ eth_client")
@@ -123,20 +68,27 @@ class FlowerClient(fl.client.EthClient):
         print("g_model_cid", g_model_cid)
         net = self.IPFSClient.get_model(g_model_cid)
         # self.set_parameters(parameters)
-        train(net, trainloader, epochs=1)
+        if sys.argv[6] == "Cifar":
+            Cifar_train(net, train_loader, epochs=1)
+        else:
+            PhysNet_train(net, train_loader, epochs=1)
+
         print('after model train')
         uploaded_cid = self.IPFSClient.add_model(self.net)
-        print('IPFS upload done',uploaded_cid)
+        print('IPFS upload done', uploaded_cid)
         tx = self.EthBase.addModelUpdate(uploaded_cid, training_round)
         self.EthBase.wait_for_tx(tx)
         print('Add Model done')
-        return [uploaded_cid], len(trainloader.dataset), {}
+        return [uploaded_cid], len(train_loader.dataset), {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        loss, accuracy = test(net, testloader)
-        log(INFO,"accuracy: %s",accuracy)
-        return loss, len(testloader.dataset), {"accuracy": accuracy}
+        if sys.argv[6] == "Cifar":
+            loss, accuracy = Cifar_test(net, test_loader, epochs=1)
+        else:
+            loss, accuracy = PhysNet_tset(net, test_loader, epochs=1)
+        log(INFO, "accuracy: %s", accuracy)
+        return loss, len(test_loader.dataset), {"accuracy": accuracy}
 
 
 # Start Flower client
